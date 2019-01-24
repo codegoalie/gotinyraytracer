f5f4b44944a6d2da2eba442db3eaf01e2bbd45e2
Chris Marshall <chris@codegoalie.com>
2019-01-24 14:19:48 -0500

    Add mirrors

--- main.go
+++ main.go
@@ -16,14 +16,19 @@ const (
 
 var (
 	ivory = &Material{
-		Albedo:           &vec2f{0.6, 0.3},
+		Albedo:           &vec3f{0.6, 0.3, 0.1},
 		DiffuseColor:     &vec3f{0.4, 0.4, 0.3},
 		SpecularExponent: 50.0,
 	}
 	redRubber = &Material{
-		Albedo:           &vec2f{0.9, 0.1},
+		Albedo:           &vec3f{0.9, 0.1, 0.0},
 		DiffuseColor:     &vec3f{0.3, 0.1, 0.1},
 		SpecularExponent: 10.0,
+	}
+	mirror = &Material{
+		Albedo:           &vec3f{0, 10, 0.8},
+		DiffuseColor:     &vec3f{1, 1, 1},
+		SpecularExponent: 1425.0,
 	}
 )
 
@@ -31,11 +36,6 @@ type vec3f struct {
 	X float64
 	Y float64
 	Z float64
-}
-
-type vec2f struct {
-	X float64
-	Y float64
 }
 
 // Sphere is represented by a vec3f center and a float64 radius
@@ -47,7 +47,7 @@ type Sphere struct {
 
 // Material describes a surface of a body
 type Material struct {
-	Albedo           *vec2f
+	Albedo           *vec3f
 	DiffuseColor     *vec3f
 	SpecularExponent float64
 }
@@ -61,9 +61,9 @@ type Light struct {
 func main() {
 	spheres := []*Sphere{
 		{Center: &vec3f{-3, 0, -16}, Radius: 2, Material: ivory},
-		{Center: &vec3f{-1, -1.5, -12}, Radius: 2, Material: redRubber},
+		{Center: &vec3f{-1, -1.5, -12}, Radius: 2, Material: mirror},
 		{Center: &vec3f{1.5, -0.5, -18}, Radius: 3, Material: redRubber},
-		{Center: &vec3f{7, 5, -18}, Radius: 4, Material: ivory},
+		{Center: &vec3f{7, 5, -18}, Radius: 4, Material: mirror},
 	}
 	lights := []*Light{
 		{Position: &vec3f{-20, 20, 20}, Intensity: 1.5},
@@ -80,7 +80,7 @@ func main() {
 		for i := 0; i < totalWidth; i++ {
 			x := (2*(float64(i)+0.5)/float64(totalWidth) - 1) * math.Tan(fov/2.0) * totalWidth / float64(totalHeight)
 			dir := (&vec3f{x, y, -1}).Normalize()
-			colorVec := castRay(&vec3f{0, 0, 0}, dir, spheres, lights)
+			colorVec := castRay(&vec3f{0, 0, 0}, dir, spheres, lights, 0)
 			rgba := color.RGBA{
 				R: uint8(math.Min(math.Max(0, colorVec.X), 1) * 255),
 				G: uint8(math.Min(math.Max(0, colorVec.Y), 1) * 255),
@@ -111,11 +111,21 @@ func sceneIntersect(orig *vec3f, dir *vec3f, spheres []*Sphere) (bool, *vec3f, *
 	return curDist < 1000, hit, n, curMaterial
 }
 
-func castRay(orig *vec3f, dir *vec3f, spheres []*Sphere, lights []*Light) *vec3f {
+func castRay(orig *vec3f, dir *vec3f, spheres []*Sphere, lights []*Light, depth int) *vec3f {
 	intersect, point, n, intersectMaterial := sceneIntersect(orig, dir, spheres)
-	if !intersect {
+	if depth > 4 || !intersect {
 		return &vec3f{55 / 255.0, 176 / 255.0, 202 / 255.0}
 	}
+
+	reflectDir := reflect(dir, n).Normalize()
+	var reflectOrig *vec3f
+	if reflectDir.Multiply(n) < 0 {
+		reflectOrig = point.Subtract(n.MultiplyF(1e-3))
+	} else {
+		reflectOrig = point.Add(n.MultiplyF(1e-3))
+	}
+	reflectColor := castRay(reflectOrig, reflectDir, spheres, lights, depth+1)
+
 	diffuseLightIntensity := 0.0
 	specularLightIntensity := 0.0
 	for _, light := range lights {
@@ -138,7 +148,9 @@ func castRay(orig *vec3f, dir *vec3f, spheres []*Sphere, lights []*Light) *vec3f
 	}
 
 	unitVec := &vec3f{1, 1, 1}
-	return intersectMaterial.DiffuseColor.MultiplyF(diffuseLightIntensity).MultiplyF(intersectMaterial.Albedo.X).Add(unitVec.MultiplyF(specularLightIntensity).MultiplyF(intersectMaterial.Albedo.Y))
+	return intersectMaterial.DiffuseColor.MultiplyF(diffuseLightIntensity).MultiplyF(intersectMaterial.Albedo.X).
+		Add(unitVec.MultiplyF(specularLightIntensity).MultiplyF(intersectMaterial.Albedo.Y)).
+		Add(reflectColor.MultiplyF(intersectMaterial.Albedo.Z))
 }
 
 // RayIntersect determines if the provided ray interescts with s.
